54159dbf49dd8ff1db9f3eb41ce8d23579122c9b
ANTIMOLE <angellokhara4672@gmail.com>
2024-09-11 12:55:25 +0700

    sixth commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Diabetes APP")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Test Drive Diabetes App"
 )
 
 st.slider("Pregnancies",0,17,(0,17))
+st.slider("Glucose",0,199,(0,199))
+st.slider("BloodPressure",0,122,(0,122))
+st.slider("SkinThickness",0,99,(0,99))
+st.slider("Insulin",0,846,(0,846))
+st.slider("BMI",0.0,67.1,(0.0,67.1))
+st.slider("DiabetesPedigreeFunction",0.078,2.42,(0.078,2.42))
+st.slider("Age",21,81,(0,81))
